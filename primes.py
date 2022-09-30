f454bc1bdd4733d9b9942ae23ec7f75864bafb05
Shantanu <shantanu.suwarnkar@kcl.ac.uk>
2022-09-30 11:36:00 +0100

    primes2

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError("Input must be greater than 0")
     list = []
+    i = 2
+    while len(list) < number_of_primes:
+        add = True
+        for j in range(2,i):
+            if i%j == 0:
+                add = False
+                break
+        if add:
+            list.append(i)
+        i += 1
     return list
+
+print(primes(1))
